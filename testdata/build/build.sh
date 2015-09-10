de7f53ac9fc772bc842a2a265bcf93d37ca6b924
ah903 <andrew.hall903@gmail.com>
2015-09-11 00:33:37 +0100

    Fix ObjectId On Test Data Load

--- testdata/build/build.sh
+++ testdata/build/build.sh
@@ -1,34 +1,68 @@
-!#/bin/bash
-
+#!/bin/bash
 # Script Variable Declarations
-# $host = 127.0.0.1
-# $port = 27017
-# $db = attire-db0-dev
-# $products = products
-# $reviews = reviews
+#host = "127.0.0.1"
+#port = "27017"
+#db = "attire-db0-dev"
+#products = "products"
+#reviews = "reviews"
 
 # Change Working Directory From Root
-cd "/Users/Andrew/Desktop/Masters/Module 5/Assignment/attire/testdata/generated"
+cd "/Users/Andrew/Desktop/Masters/Module 5/Assignment/attire/testdata/generation-02"
 
 # Clean Up Any Old Files
-rm products.json
-rm reviews.json
+rm product-100-out.json
+rm product-200-out.json
+rm product-300-out.json
+rm product-400-out.json
+
+rm review-100-out.json
+rm review-200-out.json
+rm review-300-out.json
+rm review-400-out.json
+rm review-500-out.json
+rm review-600-out.json
+rm review-700-out.json
+rm review-800-out.json
 
 # Concatenate Product Data Files
-cat product-data-*.json > products.json
+# cat product-data-*.json > products.json
+#cat product-data-100.json sep.txt product-data-200.json sep.txt product-data-300.json sep.txt product-data-400.json > products.json
 
 # Correction Script Replaces oid with $oid for managing ObjectIds in Mongo
 # JSON generator tool does not support this by default
-sed -i -e "s/oid/$oid/g" products_out.json
+sed 's/"oid":/"$oid":/g' < product-data-100.json > product-100-out.json
+sed 's/"oid":/"$oid":/g' < product-data-200.json > product-200-out.json
+sed 's/"oid":/"$oid":/g' < product-data-300.json > product-300-out.json
+sed 's/"oid":/"$oid":/g' < product-data-400.json > product-400-out.json
+
 
 # Concatenate Review Data Files
-cat review-data-*.json > reviews.json
+# cat review-data-*.json > reviews.json
+
 
 # Correction Script Replaces oid with $oid for managing ObjectIds in Mongo
 # JSON generator tool does not support this by default
-sed -i -e "s/oid/$oid/g" reviews_out.json
+sed 's/"oid":/"$oid":/g' < review-data-100.json > review-100-out.json
+sed 's/"oid":/"$oid":/g' < review-data-200.json > review-200-out.json
+sed 's/"oid":/"$oid":/g' < review-data-300.json > review-300-out.json
+sed 's/"oid":/"$oid":/g' < review-data-400.json > review-400-out.json
+sed 's/"oid":/"$oid":/g' < review-data-500.json > review-500-out.json
+sed 's/"oid":/"$oid":/g' < review-data-600.json > review-600-out.json
+sed 's/"oid":/"$oid":/g' < review-data-700.json > review-700-out.json
+sed 's/"oid":/"$oid":/g' < review-data-800.json > review-800-out.json
+
 
 # Import Products and Reviews Into Mongo Instance Dropping All Existing Data First
-#mongoimport --host $host --port $port --db $db --collection $products --drop --file products.json
-#mongoimport --host $host --port $port --db $db --collection $reviews --drop --file reviews.json
+mongoimport --host 127.0.0.1 --port 27017 --db attire-db0-dev --collection products --drop --file product-100-out.json --jsonArray
+mongoimport --host 127.0.0.1 --port 27017 --db attire-db0-dev --collection products --file product-200-out.json --jsonArray
+mongoimport --host 127.0.0.1 --port 27017 --db attire-db0-dev --collection products --file product-300-out.json --jsonArray
+mongoimport --host 127.0.0.1 --port 27017 --db attire-db0-dev --collection products --file product-400-out.json --jsonArray
 
+mongoimport --host 127.0.0.1 --port 27017 --db attire-db0-dev --collection reviews --drop --file review-100-out.json --jsonArray
+mongoimport --host 127.0.0.1 --port 27017 --db attire-db0-dev --collection reviews --file review-200-out.json --jsonArray
+mongoimport --host 127.0.0.1 --port 27017 --db attire-db0-dev --collection reviews --file review-300-out.json --jsonArray
+mongoimport --host 127.0.0.1 --port 27017 --db attire-db0-dev --collection reviews --file review-400-out.json --jsonArray
+mongoimport --host 127.0.0.1 --port 27017 --db attire-db0-dev --collection reviews --file review-500-out.json --jsonArray
+mongoimport --host 127.0.0.1 --port 27017 --db attire-db0-dev --collection reviews --file review-600-out.json --jsonArray
+mongoimport --host 127.0.0.1 --port 27017 --db attire-db0-dev --collection reviews --file review-700-out.json --jsonArray
+mongoimport --host 127.0.0.1 --port 27017 --db attire-db0-dev --collection reviews --file review-800-out.json --jsonArray
